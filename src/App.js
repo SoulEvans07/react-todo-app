efceda5f0b2bca1c145b31a10df2b0913a69ea73
soulevans07 <szi.adam@simonyi.bme.hu>
2019-09-02 17:34:22 +0200

    added event listeners

--- src/App.js
+++ src/App.js
@@ -5,6 +5,52 @@ import styles from './App.module.css'
 import task_list from './data/task_list'
 
 class App extends Component {
+  state = {
+    temp_task: "",
+    task_list: task_list
+  }
+
+  handleChange = (event) => {
+    const { name, value } = event.target
+    this.setState({
+      [name]: value
+    })
+  }
+
+  addTask = (event) => {
+    this.setState(state => {
+      const new_list = state.task_list
+      new_list.push({ done: false, text: state.temp_task })
+      return { 
+        task_list: new_list,
+        temp_task: ""
+      }
+    })
+  }
+
+  addTaskByEnter = (event) => {
+    if(event.key === 'Enter') { 
+      this.addTask()
+    }
+  }
+
+  changeTaskState = (index) => {
+    this.setState(state => {
+      const new_list = state.task_list.map((task, ind) => {
+        task.done = (ind === index ? !task.done : task.done)
+        return task
+      })
+      return { task_list: new_list }
+    })
+  }
+
+  removeTask = (index) => {
+    this.setState(state => {
+      const new_list = state.task_list.filter((task, ind) => ind !== index);
+      return { task_list: new_list }
+    })
+  }
+
   render() {
     return (
       <div className={styles.app}>
@@ -13,11 +59,15 @@ class App extends Component {
         </div>
         <div className={styles.content}>
           <div className={styles.taskList}>
-            { task_list.map((task, index) => <TaskItem task={task} index={index} key={index}/>) }
+            { this.state.task_list.map((task, index) => 
+              <TaskItem task={task} index={index} key={index} 
+                onClick={() => this.changeTaskState(index)}
+                onRemove={() => this.removeTask(index)}/>) }
 
             <div className={styles.task}>
-              <i className={`${styles.add} fa fa-plus`} />
-              <input className={styles.text} type="text"/>
+              <i className={`${styles.add} fa fa-plus`} onClick={this.addTask} />
+              <input className={styles.text} name="temp_task" type="text" 
+                value={this.state.temp_task} onChange={this.handleChange} onKeyUp={this.addTaskByEnter} />
             </div>
           </div>
         </div>
--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -5,11 +5,11 @@ class TaskItem extends Component {
   render() {
     return (
       <div className={styles.task}>
-        <div className={styles.checkbox}>
+        <div className={styles.checkbox} onClick={this.props.onClick}>
           { this.props.task.done ? <i className='fa fa-check'/> : null }
         </div>
         <div className={styles.text}>{ this.props.task.text }</div>
-        <i className={`${styles.remove} fa fa-times`} title='Delete'/>
+        <i className={`${styles.remove} fa fa-times`} title='Delete' onClick={this.props.onRemove}/>
       </div>
     )
   }
