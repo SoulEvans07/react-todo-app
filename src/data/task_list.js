5c75dd680fe12becb2450fc56462e597554e910d
soulevans07 <szi.adam@simonyi.bme.hu>
2019-09-03 09:23:26 +0200

    react flavor

--- src/data/task_list.js
+++ src/data/task_list.js
@@ -1,6 +1,7 @@
 export default [
   { done: true, text: 'Learn Javascript' },
-  { done: false, text: 'Learn ES6' },
-  { done: false, text: 'Learn Vue' },
+  { done: true, text: 'Learn ES6' },
+  { done: false, text: 'Learn React' },
+  { done: false, text: 'Learn Redux' },
   { done: false, text: 'Build something awesome!' }
 ]
