0a51204b2d64e9ce6c2b3baea319d8393b782542
soulevans07 <szi.adam@simonyi.bme.hu>
2019-09-09 10:05:30 +0200

    [wip] tag visibility fix

--- src/components/TaskTag.js
+++ src/components/TaskTag.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react'
 import './TaskTag.scss'
+import pSBC from '../helpers/pSBC'
 
 class TaskTag extends Component {
   render() {
     const tag = this.props.tag
 
+    const style = {
+      background: tag.background,
+      color: tag.color,
+      borderColor: pSBC(tag.background, -5)
+    }
+
     return (
-      <div className="taskTag" style={{background: tag.background, color: tag.color}}>
+      <div className="taskTag" style={style}>
         <div className="name">{tag.name}</div>
       </div>
     )
